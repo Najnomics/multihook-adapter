42aa8e2e186bee6244c955247c6458973b83b62e
najnomics <jesuorobonosakhare873@gmail.com>
2025-06-25 14:45:51 +0100

    removed some files

--- src/interfaces/IBaseHookExtension.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-identifiery: MIT
-pragma solidity ^0.8.0;
-
-import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
-
-interface IBaseHookExtension {
-    function getHookPermissions() external pure returns (Hooks.Permissions memory);
-}
